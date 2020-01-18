890827aa9d9d74b3fcf128b58e5b892f8da615db
Kevin <kevinihm@gmail.com>
2020-01-17 18:49:17 -0800

    removed database connection authentication in queries.js

--- server/api/queries.js
+++ server/api/queries.js
@@ -1,15 +1,8 @@
 const Pool = require('pg').Pool;
-const pool = new Pool({
-  user: 'dev',
-  host: 'localhost',
-  database: 'test',
-  password: 'lfz',
-  port: 5432
-});
+const conn = require('./db_conn');
 
 const getUsers = (req, res) => {
-  debugger;
-  pool.query('SELECT * FROM users ORDER BY id ASC', (err, results) => {
+  conn.query('SELECT * FROM users ORDER BY id ASC', (err, results) => {
     console.log(results);
     if (err) throw err;
     res.status(200).json(results.rows);
