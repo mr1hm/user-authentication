dddae5853b0f28e79c58849f6962c7f3258d4f6c
Kevin <kevinihm@gmail.com>
2020-01-17 22:14:56 -0800

    added new component welcome.jsx

---
+++ client/components/welcome.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
+
+export default class Welcome extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return null;
+  }
+}
