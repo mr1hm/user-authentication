28711dbe4fe1dd55c2b7455f38b40f00095f81e1
Kevin <kevinihm@gmail.com>
2020-01-17 18:41:23 -0800

    changed actor to user

--- server/api/queries.js
+++ server/api/queries.js
@@ -2,19 +2,21 @@ const Pool = require('pg').Pool;
 const pool = new Pool({
   user: 'dev',
   host: 'localhost',
-  database: 'pagila',
-  password: 'dev',
+  database: 'test',
+  password: 'lfz',
   port: 5432
 });
 
-const getActors = (req, res) => {
+const getUsers = (req, res) => {
+  debugger;
   pool.query('SELECT * FROM users ORDER BY id ASC', (err, results) => {
+    console.log(results);
     if (err) throw err;
     res.status(200).json(results.rows);
   })
 }
 
-const getActorById = (req, res) => {
+const getUserById = (req, res) => {
   const id = parseInt(req.params.id);
   pool.query('SELECT * FROM users WHERE id = $1', [id], (err, results) => {
     if (err) throw err;
