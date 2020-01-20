88c08e749270fcdd42c248bd5cfa4a7eb4651084
Kevin <kevinihm@gmail.com>
2020-01-19 22:02:45 -0800

    imported component fromr eact in welcome

--- client/components/welcome.jsx
+++ client/components/welcome.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Container, Row, Col } from 'reactstrap';
 
-export default class Welcome extends React.Component {
+export default class Welcome extends Component {
   constructor(props) {
     super(props);
     this.state = {
