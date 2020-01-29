e6841d23839e396bd98e725e16c2aba954b5fc60
Kevin <kevinihm@gmail.com>
2020-01-28 16:53:01 -0800

    required oauth related packages in index.js

--- server/index.js
+++ server/index.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const google = require('./api/keys');
 const app = express();
 const path = require('path');
 const staticPath = path.join(__dirname, 'public');
 const db = require('./api/queries');
 const port = 3001;
 
+passport.use(new GoogleStrategy({
+  clientID: google.clientID,
+  clientSecret: google.clientSecret,
+  callbackURL: 'http://localhost:3000/auth/google/callback'
+}, (accessToken, refreshToken, profile, done) => {
+  User.findOrCreate({ googleID: profile.id }, (err, user) => {
+    return done(err, user);
+  })
+}));
 app.use(express.static(staticPath));
 app.use(express.json());
 
